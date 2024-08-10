3a08a63dc31ca47f72f84e061094c5648f07b5d7
lonzzi <lonzzi@qq.com>
2024-08-11 01:47:52 +0800

    feat: add auto checkin

---
+++ types.ts
@@ -0,0 +1,17 @@
+export type Response<T> = {
+  err_no: number;
+  err_msg: string;
+  data: T[];
+};
+
+export type UserMonthlyActivity = Response<{
+  date: number;
+  status: number;
+  point: number;
+}>;
+
+export type UserCheckInStatus = Response<{
+  check_in_done: boolean;
+  lt_task_exist: boolean;
+  extra: string | null;
+}>;
